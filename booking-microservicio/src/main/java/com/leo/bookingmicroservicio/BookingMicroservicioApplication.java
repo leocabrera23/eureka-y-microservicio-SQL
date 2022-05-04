300a7f750d900b6840ba5a995790e927313ebce1
leocabrera23 <83379102+leocabrera23@users.noreply.github.com>
2022-05-04 18:45:08 -0300

    commit 1

---
+++ booking-microservicio/src/main/java/com/leo/bookingmicroservicio/BookingMicroservicioApplication.java
@@ -0,0 +1,15 @@
+package com.leo.bookingmicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BookingMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMicroservicioApplication.class, args);
+	}
+
+}
